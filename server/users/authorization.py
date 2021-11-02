89cb040f8432b87dce2695899338c71f6371c679
qzart45hi <qwert45hi@yandex.ru>
2021-11-02 17:54:06 +0300

    feat(back): implementing authorization in reglog endpoints

--- server/users/authorization.py
+++ server/users/authorization.py
@@ -1,7 +1,7 @@
 from flask_restx import Namespace, Resource
 from flask_restx.reqparse import RequestParser
 
-from add import argument_parser, with_session
+from add import argument_parser, with_session, add_auth_cookies
 from .database import User
 
 users_namespace: Namespace = Namespace("users", path="/")
@@ -9,25 +9,29 @@ users_namespace: Namespace = Namespace("users", path="/")
 
 @users_namespace.route("/sign-up/")
 class SignUp(Resource):
-    parser: RequestParser = RequestParser
-    parser.add_argument("email", type=str, required=True)
-    parser.add_argument("password", type=str, required=True)
+    parser: RequestParser = RequestParser()
+    parser.add_argument("email", required=True)
+    parser.add_argument("password", required=True)
 
     @with_session
     @argument_parser(users_namespace, parser)
-    def post(self, session, email: str, password: str) -> bool:
-        return User.create(session, email, password) is not None
+    @add_auth_cookies
+    def post(self, session, email: str, password: str):
+        return (user := User.create(session, email, password)) is not None, user
 
 
 @users_namespace.route("/sign-in/")
 class SignIn(Resource):
-    parser: RequestParser = RequestParser
-    parser.add_argument("email", type=str, required=True)
-    parser.add_argument("password", type=str, required=True)
+    parser: RequestParser = RequestParser()
+    parser.add_argument("email", required=True)
+    parser.add_argument("password", required=True)
 
     @with_session
     @argument_parser(users_namespace, parser)
+    @add_auth_cookies
     def post(self, session, email: str, password: str):
         if (user := User.find_by_email(session, email)) is None:
-            return "User does not exist", 404
-        return User.verify_hash(password, user.password)
+            return "user does not exist", 404, user
+        if User.verify_hash(password, user.password):
+            return "success", user
+        return "wrong password", user
