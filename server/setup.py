21caaefdc75c333ba68d4e32babb10b3025f4e70
qzart45hi <qwert45hi@yandex.ru>
2022-01-01 22:21:08 +0300

    feat: install & configure flask_mailing with dotenv

--- server/setup.py
+++ server/setup.py
@@ -1,10 +1,14 @@
 from datetime import timedelta
 from os import getenv
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
+from flask_mail import Mail
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
 
 app: Flask = Flask(__name__, static_folder="../client/build", static_url_path="")
 app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -15,6 +19,21 @@ app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access"]
 
 for secret_name in ["SECRET_KEY", "JWT_SECRET_KEY"]:
     app.config[secret_name] = getenv(secret_name, "hope it's local")
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+for setting_name in ["MAIL_USERNAME", "MAIL_PASSWORD"]:
+    app.config[setting_name] = getenv(setting_name)
+app.config["MAIL_DEFAULT_SENDER"] = app.config["MAIL_USERNAME"]
+
+mail = Mail(app)
+
+# with app.app_context():
+#     from flask_mail import Message
+#     mail.send(Message("Hello", ["qwert45hi@yandex.ru"], body="Hey, how are you?", html="<h1>I'm beautiful</h1><br>"))
+#     exit()
 
 cors = CORS(app)
 
